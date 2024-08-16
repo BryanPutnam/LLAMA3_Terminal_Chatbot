a3291ff8f5a7202c82847f27e69d44ce53ebd2a6
Bryan Putnam <bryanputnam25@gmail.com>
2024-08-16 15:33:09 -0700

    Added separate file for redis operations

--- script.py
+++ script.py
@@ -1,14 +1,10 @@
 # Tells OS to run as python executible 
 #!/usr/bin/env python3
 import os
-
-# Import Redis Database info from config.json
-import json
-import redis
+import sys
 
 # Figure it out 
 import signal
-import sys
 
 # Library for terminal width to calculate separation barrier
 import shutil
@@ -20,6 +16,11 @@ from rich.markdown import Markdown
 # Library for Groq
 from groq import Groq
 
+# Import functions from REDIS_FLOW (redis_db.py)
+redis_flow_path = os.environ.get('REDIS_FLOW')
+sys.path.insert(0, os.path.dirname(redis_flow_path))
+from redis_db import get_redis_client, handle_exit
+
 # Initialize console for markdown
 console = Console()
 
@@ -29,82 +30,9 @@ conversation_history = []
 # Initialize counter for prompt message
 prompt_counter = 0
 
-# Global redis client 
-redis_client = None
-
 def get_api_key(): 
     API_KEY = os.getenv('GROQ_API_KEY')
     return API_KEY
-
-def get_redis_client():
-    global redis_client
-    if redis_client is None:
-        config_path = os.getenv('CONFIG_PATH', 'config.json')
-        with open(config_path, 'r') as file: 
-            config = json.load(file)
-        redis_endpoint = config['redis']['endpoint']
-        redis_password = config['redis']['password']
-        
-        try:
-            redis_client = redis.Redis(host=redis_endpoint, port=18656, password=redis_password)
-            print("Connecting to Redis...")
-        except Exception as err:
-            print(f"Error connecting to Redis: {err}")
-            sys.exit(1)
-    # Load conversation history after successful connection to database
-    load_conversation_history()
-    return redis_client
-
-def flatten_list(nested_list):
-    flat_list = []
-    for item in nested_list:
-        if isinstance(item, list):
-            flat_list.extend(flatten_list(item))
-        else:
-            flat_list.append(item)
-    return flat_list
-
-def load_conversation_history(): 
-    global redis_client
-    try: 
-        if redis_client: 
-            # Retrieve all elements from the Redis list 
-            data = redis_client.lrange("conversation_history_list", 0, -1)
-            # Load data into conversation history (load in place)
-            flat_data = flatten_list([json.loads(item.decode('utf-8')) for item in data if item])
-            if(flat_data != []):
-                conversation_history[:] = flat_data
-                print("Loading Conversation History...")
-            else: print("No Available History...")
-    except Exception as e:
-        print(f"Failed to load conversation history: {e}")
-        
-def push_conversation_history():
-    try:
-        if redis_client:
-            if conversation_history:
-                # Push as list
-                redis_client.rpush("conversation_history_list", json.dumps(conversation_history))
-                print("Pushing to Redis...")
-            else: 
-                print("Conversation Empty. Skipping Push...")
-    except Exception as e:
-        print(f"Failed to push to Redis: {e}", redis_client)
-
-def handle_exit(signal, frame):
-    global redis_client
-    if redis_client:
-        print("Closing Redis Connection...")
-        try:
-            push_conversation_history()
-            redis_client.close()
-        except Exception as e:
-            print(f"Error during Redis client cleanup: {e}")
-        finally:
-            redis_client = None  # Ensure it's not referenced again
-            print("Finished")
-    print("=" * getTermWidth())
-    sys.exit(0)
 
 def getTermWidth(): 
     term_width = shutil.get_terminal_size().columns
@@ -156,7 +84,7 @@ def conversation(prompt_counter):
         })
     
 if __name__ == "__main__":
-    #get_redis_client() # Comment this out if you do not with to use Redis AT ALL (No connection, loaded history, or pushed data)
+    get_redis_client(conversation_history) # Comment this out if you do not with to use Redis AT ALL (No connection, loaded history, or pushed data)
     try: 
         while True:
             conversation(prompt_counter)
@@ -164,5 +92,6 @@ if __name__ == "__main__":
     except KeyboardInterrupt: 
         print("\nGoodbye!")
         print("=" * getTermWidth())
-        handle_exit(signal.SIGINT, None)
+        term_width = getTermWidth()
+        handle_exit(signal.SIGINT, None, term_width, conversation_history)
     
