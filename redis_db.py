0830f108c2960b78c4e90135b15d3516bbbe9b35
Bryan Putnam <bryanputnam25@gmail.com>
2024-08-20 11:08:38 -0700

    Changed data structure for db. Code cleanup

--- redis_db.py
+++ redis_db.py
@@ -4,6 +4,8 @@ import json
 import os
 import sys
 import redis 
+import time 
+import secrets
 
 # Library for rich markdown conversion
 from rich.console import Console
@@ -13,6 +15,8 @@ console = Console()
 
 # Global redis client 
 redis_client = None
+
+list_key = ""
 
 def get_redis_client(conversation_history):
     global redis_client
@@ -33,6 +37,14 @@ def get_redis_client(conversation_history):
     load_conversation_history(conversation_history)
     return redis_client
 
+def create_session_list(): 
+    epoch_time = int(time.time()) # Store timestamp
+    salt = secrets.token_hex(16)
+    session_id = f"{epoch_time:x}{salt}"
+    
+    list_key = f"session:{session_id}"
+    return list_key 
+    
 def flatten_list(nested_list):
     flat_list = []
     for item in nested_list:
@@ -42,27 +54,75 @@ def flatten_list(nested_list):
             flat_list.append(item)
     return flat_list
 
-def load_conversation_history(conversation_history): ## ADDED PARAM
+def process_data(data):
+    processed_data = []
+    for item in data:
+        if isinstance(item, list):
+            processed_data.extend(process_data(item)) # Recursively process items within the list
+        elif isinstance(item, bytes):
+            decoded_item = item.decode('utf-8') # Decode bytes to string
+            json_data = json.loads(decoded_item) # Convert JSON string to Python object
+            processed_data.append(json_data)
+        else:
+            processed_data.append(item) # Append item directly if it's already in the correct format
+
+    flat_data = flatten_list(processed_data) # Flatten the processed data
+    return flat_data
+
+def get_last_session_keys(redis_client, num_values): 
+    if(num_values != 3): 
+        last_sessions = redis_client.lrange("Key_List", 0, -1) # Dumps all sessions
+    else: 
+        last_sessions = redis_client.lrange("Key_List", 0, num_values - 1)
+    last_sessions = [item.decode('utf-8') for item in last_sessions]
+    return last_sessions
+
+def get_last_session_values(redis_client, keys): 
+    values = []
+    for key in keys: 
+        list_values = redis_client.lrange(key, 0, -1)
+        values.append(list_values)
+    return values
+
+def add_key(session_key): 
     global redis_client
+    redis_client.lpush("Key_List", session_key)
+    redis_client.ltrim("Key_List", 0, 99) # Only keep last 100 keys
+
+def load_conversation_history(conversation_history):
+    global redis_client # YOU MIGHT BE ABLE TO GET RID OF THIS??? (PASS AS PARAM)
     try: 
         if redis_client: 
-            # Retrieve all elements from the Redis list 
-            data = redis_client.lrange("conversation_history_list", 0, -1)
+            data = [] # Initialize 
+            # Retrieve last 3 elements from the Redis list 
+            if((redis_client.execute_command('DBSIZE') - 1) >= 3):
+                try:
+                    last_three_keys = get_last_session_keys(redis_client, 3)
+                    last_three_vals = get_last_session_values(redis_client, last_three_keys)
+                    data = last_three_vals
+                except Exception as e: 
+                    console.print(f"[bold red]WARNING:[/bold red] Error retrieving from Redis: {e}")
+            else: 
+                try:  
+                    all_keys = get_last_session_keys(redis_client, 0)
+                    all_vals = get_last_session_values(redis_client, all_keys)
+                    data = all_vals
+                except Exception as e: 
+                    console.print(f"[bold red]WARNING:[/bold red] Error retrieving from Redis: {e}")
             # Load data into conversation history (load in place)
-            flat_data = flatten_list([json.loads(item.decode('utf-8')) for item in data if item])
-            if(flat_data != []):
+            flat_data = process_data(data)
+            if(flat_data):
                 conversation_history[:] = flat_data
                 print("Loading Conversation History...")
             else: print("No Available History...")
     except Exception as e:
         console.print(f"[bold red]WARNING:[/bold red] Failed to load Redis data: No previously stored data will be available moving forward. \n {e}")
         
-def push_conversation_history(conversation_history): ## ADDED PARAM
+def push_conversation_history(qa_segment, list_key):
     try:
         if redis_client:
-            if conversation_history:
-                # Push as list
-                redis_client.rpush("conversation_history_list", json.dumps(conversation_history))
+            if qa_segment:
+                redis_client.rpush(list_key, json.dumps(qa_segment)) # Push as list
                 print("Pushing to Redis...")
             else: 
                 print("Conversation Empty. Skipping Push...")
@@ -74,7 +134,6 @@ def handle_exit(signal, frame, conversation_history, term_width):
     if redis_client:
         print("Closing Redis Connection...")
         try:
-            push_conversation_history(conversation_history)
             redis_client.close()
         except Exception as e:
             console.print(f"[bold red]WARNING:[/bold red] Error during Redis client cleanup: {e}")
@@ -83,3 +142,9 @@ def handle_exit(signal, frame, conversation_history, term_width):
             print("Finished")
     print("=" * term_width)
     sys.exit(0)
+    
+    
+    
+    ########
+    # YOU NEED TO MAKE SURE THAT THE SESSION KEY IS NOT PUSHED INTO THE KEY_LIST LIST IF DATA 1) FAILS TO PUSH 
+    # TO REDIS, OR 2) FORCEFULLY DOES NOT PUSH BECAUSE OF EMPTY QA_SEGMENT
--- script.py
+++ script.py
@@ -19,7 +19,7 @@ from groq import Groq
 # Import functions from REDIS_FLOW (redis_db.py)
 redis_flow_path = os.environ.get('REDIS_FLOW')
 sys.path.insert(0, os.path.dirname(redis_flow_path))
-from redis_db import get_redis_client, handle_exit
+from redis_db import get_redis_client, handle_exit, push_conversation_history, create_session_list, add_key
 
 # Initialize console for markdown
 console = Console()
@@ -41,15 +41,19 @@ def getTermWidth():
 def prompt(prompt_counter): 
     print("=" * getTermWidth())
     if(prompt_counter < 1): 
-        #print("=" * getTermWidth())
         user_input = input("What would you like help with today?\n > ")
     else: 
         user_input = input("\n > ")
     
     return user_input
 
+def append_list(list, response): ### NEW FUNCTIONS FOR LIST APPENDS (MAKE SURE IT WORKS)
+    list.append({ 
+            "role": "user", 
+            "content": response
+            })
 
-def conversation(prompt_counter): 
+def conversation(prompt_counter, list_key): 
     API_KEY = get_api_key()
     
     try:
@@ -57,8 +61,7 @@ def conversation(prompt_counter):
     except Exception as e: 
         console.print(f"[bold red]WARNING:[/bold red] Failed to connect to Groq. Please check your API KEY and/or Network Connnection") 
 
-    # Append the user's input to the converstation history
-    conversation_history.append({ 
+    conversation_history.append({ # Append the user's input to the converstation history
         "role": "user", 
         "content": prompt(prompt_counter)
         })
@@ -82,23 +85,27 @@ def conversation(prompt_counter):
             response += response_chunk
         markdown = Markdown(response)
         console.print(markdown, end="")
-    
-        conversation_history.append({ 
-            "role": "user", 
-            "content": response
-            })
+
+        append_list(conversation_history, response)
+        qa_segment = [] # Reset qa_segment every question/answer iteration and then store info to be pushed.
+        append_list(qa_segment, response)
+        
+        push_conversation_history(qa_segment, list_key) # Push qa_segment to Redis
+        
     except Exception as e: 
-        console.print(f"[bold red]WARNING:[/bold red] Unable to Complete Request: Check Groq Server Status")
+        console.print(f"[bold red]WARNING:[/bold red] Unable to Complete Request: Check Groq Server Status: {e}")
         sys.exit(0)
     
 if __name__ == "__main__":
-    get_redis_client(conversation_history) # Comment this out if you do not with to use Redis AT ALL (No connection, loaded history, or pushed data)
+    get_redis_client(conversation_history) # Comment this out if you do not wish to use Redis AT ALL (No connection, loaded history, or pushed data)
+    list_key = create_session_list()
+    add_key(list_key)
     try: 
         while True:
-            conversation(prompt_counter)
+            conversation(prompt_counter, list_key)
             prompt_counter += 1
     except KeyboardInterrupt: 
         print("\nGoodbye!")
         print("=" * getTermWidth())
         handle_exit(signal.SIGINT, None, conversation_history, term_width=getTermWidth())
-    
+        
