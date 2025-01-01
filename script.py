9942ad880316404678bcd758271f28ec5c05aa6b
Bryan Putnam <bryanputnam25@gmail.com>
2025-01-01 12:05:00 -0800

    Updated Groq model

--- script.py
+++ script.py
@@ -88,7 +88,7 @@ def conversation(prompt_counter):
     
     try:
         completion = client.chat.completions.create(
-            model="llama-3.1-70b-versatile",
+            model="llama-3.3-70b-versatile",
             messages=conversation_history,
             temperature=1,
             max_tokens=1024,
